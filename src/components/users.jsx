56712904727b0ff3febc609f8f108332c3207e70
Dmitriy Bratishchev <dmitriy-1008@yandex.ru>
2021-09-15 13:41:22 +0300

    add groupList, feltered users

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,60 +1,98 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Pagination from "./pagination";
 import { paginate } from "../utils/paginate";
 import HeaderSumUsers from "./tableUsers/headerSumUsers";
 import RowUser from "./tableUsers/rowUser";
+import GroupList from "./groupList";
+import API from "../api";
 
 const Users = ({ handleDelete, handleFavorit, users: allUsers }) => {
-  const [ curentPage, setCurentPage ] = useState(1);
+  const [curentPage, setCurentPage] = useState(1);
+  const [professions, setProfessions] = useState(null);
+  const [selectedProf, setSelectedProf] = useState();
+
+  const pageSize = 4;
+
+  useEffect(() => {
+    API.professions.fetchAll().then((res) => setProfessions(res));
+  }, []);
+
+  useEffect(() => {
+    setCurentPage(1);
+  }, [selectedProf]);
+
   const renderFavorit = () => {
     return allUsers.filter((el) => el.favorit).length;
   };
 
-  const countUsers = allUsers.length;
-  const pageSize = 4;
+  const hendelProfessionSelect = (item) => {
+    setSelectedProf(item);
+  };
 
   const hendelPageChange = (pageIndex) => {
-    console.log(pageIndex);
     setCurentPage(pageIndex);
   };
 
-  const users = paginate(allUsers, curentPage, pageSize);
+  const filteredUsers = selectedProf
+    ? allUsers.filter((user) => user.profession.name === selectedProf)
+    : allUsers;
+  const countUsers = filteredUsers.length;
+  const users = paginate(filteredUsers, curentPage, pageSize);
+  const clearSelected = () => {
+    setSelectedProf();
+  };
 
   return (
-    <div>
-      <HeaderSumUsers sum={countUsers} />
-      {countUsers !== 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col">в избранном: {renderFavorit()}</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <RowUser
-                key={user._id}
-                user={user}
-                handleDelete={handleDelete}
-                handleFavorit={handleFavorit}
-              />
-            ))}
-          </tbody>
-        </table>
+    <div className="d-flex">
+      {professions && (
+        <div className="d-flex flex-column flex-shrink-0 p-3">
+          <GroupList
+            selectedItem={selectedProf}
+            items={professions}
+            onItemSelect={hendelProfessionSelect}
+          />
+          <button className="btn btn-secondary pt-2" onClick={clearSelected}>
+            Сбросить фильтр
+          </button>
+        </div>
       )}
-      <Pagination
-        itemsCount={countUsers}
-        pageSize={pageSize}
-        curentPage={curentPage}
-        onPageChange={hendelPageChange}
-      />
+      <div className="d-flex flex-column">
+        <HeaderSumUsers sum={countUsers} />
+        {countUsers !== 0 && (
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Встретился, раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col">в избранном: {renderFavorit()}</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <RowUser
+                  key={user._id}
+                  user={user}
+                  handleDelete={handleDelete}
+                  handleFavorit={handleFavorit}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
+        <div className="d-flex justify-content-center">
+          <Pagination
+            itemsCount={countUsers}
+            pageSize={pageSize}
+            curentPage={curentPage}
+            onPageChange={hendelPageChange}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/particales/renderPhrase.js
+++ src/particales/renderPhrase.js
@@ -1,10 +1,10 @@
 export const renderPhrase = (length) => {
   const n = length % 10;
-  const exception = [ 11, 12, 13, 14 ];
+  const exception = [11, 12, 13, 14];
   if (exception.includes(length % 100)) {
     return "человек тусанут";
   } else if (n === 1) {
     return "человек тусанёт";
   }
-  return n === 2 || n === 3 || n === 4 ? "человека тусанёт" : "человек тусанут";
+  return n === 2 || n === 3 || n === 4 ? "человека тусанут" : "человек тусанут";
 };
